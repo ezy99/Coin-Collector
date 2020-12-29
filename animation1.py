8bbccc4ce6d029b2deb9e6f48b4c800f8b148e1d
ezy99 <76715869+ezy99@users.noreply.github.com>
2020-12-29 10:28:02 +0800

    Add files via upload

---
+++ animation1.py
@@ -0,0 +1,303 @@
+import random
+import arcade
+import os
+
+SPRITE_SCALING = 0.5
+SPRITE_SCALING_COIN = 0.5
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "The Coin Collector"
+
+# Minimum margin between the character and the edge of the screen.
+VIEWPORT_MARGIN = 40
+
+# Movement speed and frame rate
+MOVEMENT_SPEED = 8
+UPDATES_PER_FRAME = 5
+
+# Physics
+JUMP_SPEED = 25 * SPRITE_SCALING
+GRAVITY = .9 * SPRITE_SCALING
+
+# Constants used to track if the player is facing left or right
+RIGHT_FACING = 0
+LEFT_FACING = 1
+
+#set number of coins
+NUMBER_OF_COINS = 50
+
+def load_texture_pair(filename):
+    """
+    Load a texture pair, with the second being a mirror image.
+    """
+    return [
+        arcade.load_texture(filename),
+        arcade.load_texture(filename, flipped_horizontally=True)
+    ]
+
+class PlayerCharacter(arcade.Sprite):
+    def __init__(self):
+
+        # Set up parent class
+        super().__init__()
+
+        # Default to face-right
+        self.character_face_direction = RIGHT_FACING
+
+        # Used for flipping between image sequences
+        self.cur_texture = 0
+
+        self.scale = SPRITE_SCALING
+
+        # Adjust the collision box. Default includes too much empty space
+        # side-to-side. Box is centered at sprite center, (0, 0)
+        self.points = [[-22, -64], [22, -64], [22, 28], [-22, 28]]
+
+        # --- Creating moving animation ---
+
+        # Image Source
+        main_path = ":resources:images/animated_characters/robot/robot"
+
+        # Load textures for idle standing
+        self.idle_texture_pair = load_texture_pair(f"{main_path}_idle.png")
+
+        # Load textures for walking
+        self.walk_textures = []
+        for i in range(8):
+            texture = load_texture_pair(f"{main_path}_walk{i}.png")
+            self.walk_textures.append(texture)
+
+    def update_animation(self, delta_time: float = 1/60):
+
+        # Check the face should be facing left or right
+        if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
+            self.character_face_direction = LEFT_FACING
+        elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
+            self.character_face_direction = RIGHT_FACING
+
+        # Idle animation
+        if self.change_x == 0 and self.change_y == 0:
+            self.texture = self.idle_texture_pair[self.character_face_direction]
+            return
+
+        # Walking animation
+        self.cur_texture += 1
+        if self.cur_texture > 7 * UPDATES_PER_FRAME:
+            self.cur_texture = 0
+        frame = self.cur_texture // UPDATES_PER_FRAME
+        direction = self.character_face_direction
+        self.texture = self.walk_textures[frame][direction]
+
+
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self, width, height, title):
+        """
+        Initializer
+        """
+        super().__init__(width, height, title)
+
+        # Set the working directory (where we expect to find files) to the same
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
+        # Sprite lists
+        self.player_list = None
+        self.player_sprite = None
+        self.coin_list = None
+        self.wall_list = None
+        self.score = 0
+
+        self.physics_engine = None
+
+        # Used in scrolling
+        self.view_bottom = 0
+        self.view_left = 0
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+        self.score = 0
+
+        # Set up the player
+        self.player_sprite = PlayerCharacter()
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 64
+        self.player_list.append(self.player_sprite)
+
+        # Set up several rows of walls
+        for y in range(0, 768, 128):
+            for x in range(0, 896, 64):
+                p = random.randrange(1)
+                q = random.randrange(100)
+                # Randomly skip a box so the player can find a way through
+                if p > 0 or q < 70:
+                    wall = arcade.Sprite(":resources:images/tiles/brickGrey.png", SPRITE_SCALING)
+                    wall.center_x = x
+                    wall.center_y = y
+                    self.wall_list.append(wall)
+        # Make a square wall
+        # Two parallel column
+        z = [0, 896]
+        for x in z:
+            for y in range(50, 768, 64):
+                wall = arcade.Sprite(":resources:images/tiles/brickGrey.png", SPRITE_SCALING)
+                wall.center_x = x
+                wall.center_y = y
+                self.wall_list.append(wall)
+        # Two parallel row
+        k = [0, 768]
+        for y in k:
+            for x in range(0, 896, 64):
+                wall = arcade.Sprite(":resources:images/tiles/brickGrey.png", SPRITE_SCALING)
+                wall.center_x = x
+                wall.center_y = y
+                self.wall_list.append(wall)
+
+        # Create the coins
+        for i in range(NUMBER_OF_COINS):
+
+            # Create the coin instance
+            coin = arcade.Sprite(":resources:images/items/coinGold.png", SPRITE_SCALING_COIN)
+
+            # Boolean variable if we successfully placed the coin
+            coin_placed_successfully = False
+
+            # Keep trying until success
+            while not coin_placed_successfully:
+                # Position the coin
+                coin.center_x = random.randrange(SCREEN_WIDTH)
+                coin.center_y = random.randrange(SCREEN_HEIGHT)
+
+                # Check the coin is not collide with wall
+                wall_hit_list = arcade.check_for_collision_with_list(coin, self.wall_list)
+
+                # Check the coin is not overlap with another coin
+                coin_hit_list = arcade.check_for_collision_with_list(coin, self.coin_list)
+
+                if len(wall_hit_list) == 0 and len(coin_hit_list) == 0:
+                    coin_placed_successfully = True
+
+            # Add the coin to the lists
+            self.coin_list.append(coin)
+
+        self.physics_engine = \
+            arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, gravity_constant=GRAVITY)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.YANKEES_BLUE)
+
+        # Set the viewport boundaries
+        # These numbers set where we have 'scrolled' to.
+        self.view_left = 0
+        self.view_bottom = 0
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = JUMP_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Call update on all sprites
+        self.physics_engine.update()
+
+        # Update the players animation
+        self.player_list.update_animation()
+
+        # Update on the coins
+        self.coin_list.update()
+
+        # Generate a list of all sprites that collided with the player.
+        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+
+        # Update on the score
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+
+        # Loop through each colliding sprite and remove it
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
+
+        # --- Manage Scrolling ---
+        changed = False
+
+        # Scroll left
+        left_boundary = self.view_left + VIEWPORT_MARGIN
+        if self.player_sprite.left < left_boundary:
+            self.view_left -= left_boundary - self.player_sprite.left
+            changed = True
+
+        # Scroll right
+        right_boundary = self.view_left + SCREEN_WIDTH - VIEWPORT_MARGIN
+        if self.player_sprite.right > right_boundary:
+            self.view_left += self.player_sprite.right - right_boundary
+            changed = True
+
+        # Scroll up
+        top_boundary = self.view_bottom + SCREEN_HEIGHT - VIEWPORT_MARGIN
+        if self.player_sprite.top > top_boundary:
+            self.view_bottom += self.player_sprite.top - top_boundary
+            changed = True
+
+        # Scroll down
+        bottom_boundary = self.view_bottom + VIEWPORT_MARGIN
+        if self.player_sprite.bottom < bottom_boundary:
+            self.view_bottom -= bottom_boundary - self.player_sprite.bottom
+            changed = True
+
+        self.view_left = int(self.view_left)
+        self.view_bottom = int(self.view_bottom)
+
+        if changed:
+            arcade.set_viewport(self.view_left,
+                                SCREEN_WIDTH + self.view_left,
+                                self.view_bottom,
+                                SCREEN_HEIGHT + self.view_bottom)
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        arcade.start_render()
+
+        # Draw all the sprites.
+        self.wall_list.draw()
+        self.player_list.draw()
+        self.coin_list.draw()
+
+        # Put the text on the screen.
+        output = f"Score: {self.score}"
+        left_boundary = self.view_left + VIEWPORT_MARGIN
+        bottom_boundary = self.view_bottom + VIEWPORT_MARGIN
+        arcade.draw_text(output, left_boundary - 25, bottom_boundary - 17, arcade.color.COSMIC_LATTE, 16)
+
+def main():
+    """ Main method """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
